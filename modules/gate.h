8b039bf6477ab696f4857057061276e34935f711
fischy8 <115506183+fischy8@users.noreply.github.com>
2023-02-14 23:11:22 -0500

    Add files via upload

---
+++ modules/gate.h
@@ -0,0 +1,17 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _GATE_H_
+#define _GATE_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void gateInit();
+void gateOpenClose();
+
+//=====[#include guards - end]=================================================
+
+#endif // _GATE_H_
